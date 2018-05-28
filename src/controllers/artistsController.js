9c777b3dbf6dff7aabb292e1357044f2df2f9919
Igor Busquets <igor.busquets@altran.com>
2018-05-28 15:41:54 +0100

    Added validation of artist id lenght for Update and Delete

--- src/controllers/artistsController.js
+++ src/controllers/artistsController.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose'),
-Artists = mongoose.model('Artists')
+    Artists = mongoose.model('Artists')
 
 //CRUD methods
 
 //GET ALL
-exports.artists = function(req, res) {
-    Artists.find({}, function(err, artists) {
+exports.artists = function (req, res) {
+    Artists.find({}, function (err, artists) {
         if (err) {
             res.send(err);
         } else {
@@ -15,10 +15,10 @@ exports.artists = function(req, res) {
 };
 
 //GET SINGLE BY ID
-exports.getSingleArtist = function(req, res) {
+exports.getSingleArtist = function (req, res) {
     var artistId = String(req.params.id);
     if (artistId.length === 24) {
-        Artists.findById(mongoose.Types.ObjectId(artistId), function(err, artist) {
+        Artists.findById(mongoose.Types.ObjectId(artistId), function (err, artist) {
             if (err) {
                 res.send("Artist not found");
             } else {
@@ -31,9 +31,9 @@ exports.getSingleArtist = function(req, res) {
 };
 
 //CREATE
-exports.add = function(req, res) {
+exports.add = function (req, res) {
     var newArtist = new Artists(req.body);
-    newArtist.save(function(err, artist) {
+    newArtist.save(function (err, artist) {
         if (err) {
             res.send(err);
         } else {
@@ -43,29 +43,37 @@ exports.add = function(req, res) {
 };
 
 //UPDATE
-exports.update = function(req, res) {
-    var artistId = String(req.params.id);    
-    var id = mongoose.Types.ObjectId(artistId);
-    Artists.findOneAndUpdate({ _id: id }, req.body, { new: true }, function(err, artist) {
-        if (err) {
-            res.send(err);
-        }else{
-            res.json(artist);
-        }
-    });
+exports.update = function (req, res) {
+    var artistId = String(req.params.id);
+    if (artistId.length === 24) {
+        var id = mongoose.Types.ObjectId(artistId);
+        Artists.findOneAndUpdate({ _id: id }, req.body, { new: true }, function (err, artist) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json(artist);
+            }
+        });
+    } else {
+        res.send("request Id is invalid. \nid received: " + artistId);
+    }
 };
 
 //DELETE
-exports.delete = function(req, res) {
+exports.delete = function (req, res) {
     var artistId = String(req.params.id);
-    var id = mongoose.Types.ObjectId(artistId);
-    Artists.remove({
-        _id: id
-    }, function(err, artist) {
-        if (err) {
-            res.send(err);
-        }else{
-            res.json({ message: 'artist successfully deleted' , _id: id});
-        }
-    });
+    if (artistId.length === 24) {
+        var id = mongoose.Types.ObjectId(artistId);
+        Artists.remove({
+            _id: id
+        }, function (err, artist) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json({ message: 'artist successfully deleted', _id: id });
+            }
+        });
+    } else {
+        res.send("request Id is invalid. \nid received: " + artistId);
+    }
 };
